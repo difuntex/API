7a2d2f243e76ede5877f51ed618c9b26f763a919
Rafael Teodoro do Nascimento <rafael.teodoro.do.nascimento@gmail.com>
2021-10-22 09:20:42 -0300

    Criando o controller, o model e a migration de produtos

---
+++ database/migrations/1634904561517_produtos_schema.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class ProdutosSchema extends Schema {
+  up() {
+    this.create("produtos", (table) => {
+      table.increments();
+      table.timestamps();
+      table.string("nome").notNullable();
+      table.string("descricao").notNullable();
+      table.float("preco").notNullable();
+      table.integer("vendendo").notNullable();
+    });
+  }
+
+  down() {
+    this.drop("produtos");
+  }
+}
+
+module.exports = ProdutosSchema;
